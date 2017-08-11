8ffef5d9f3b0e53303d12618f74db7d4898475b7
Constanza Madrigal Reyes <madrigalreyes.c@gmail.com>
2017-08-10 19:46:30 -0500

    Hello World with arguments.

--- hello-world.c
+++ hello-world.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
-int main(){
-  printf("Hello World\n");
+// int main(){
+//   printf("Hello World\n");
+//   return 0;
+// }
+
+int main(int argn, char ** argv){
+  int i;
+  for(i=1; i<argn ; i++){
+    //The first argument is my executable file name
+    printf("Hello World %s \n", argv[i]);
+  }
   return 0;
 }
